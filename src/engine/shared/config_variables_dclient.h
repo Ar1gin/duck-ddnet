5a51d254a7542c1a6103bc863713898cb8084621
Ar1gin <79476345+Ar1gin@users.noreply.github.com>
2025-04-01 14:58:06 +0500

    Mod rebase

---
+++ src/engine/shared/config_variables_dclient.h
@@ -0,0 +1,11 @@
+
+// All DClient variables are defined here
+
+MACRO_CONFIG_INT(DcShowFlags, dc_show_flags, 0, 0, 1, CFGFLAG_CLIENT | CFGFLAG_SAVE, "Tee stats (Deep/Jetpack/etc)")
+MACRO_CONFIG_INT(DcShowFlagsSize, dc_show_flags_size, 30, -50, 100, CFGFLAG_CLIENT | CFGFLAG_SAVE, "Size of tee stat indicators")
+MACRO_CONFIG_INT(DcGrenadePath, dc_grenade_path, 0, 0, 1, CFGFLAG_CLIENT | CFGFLAG_SAVE, "Grenade path prediction")
+MACRO_CONFIG_INT(DcLaserPath, dc_laser_path, 0, 0, 1, CFGFLAG_CLIENT | CFGFLAG_SAVE, "Laser path prediction")
+MACRO_CONFIG_INT(DcShowDJ, dc_show_jumps, 0, 0, 1, CFGFLAG_CLIENT | CFGFLAG_SAVE, "Show remaining double jumps of a tee")
+MACRO_CONFIG_INT(DcShowJumpsSize, dc_show_jumps_size, 30, -50, 100, CFGFLAG_CLIENT | CFGFLAG_SAVE, "Size of double jump indicators")
+MACRO_CONFIG_INT(DcFreeMouse, dc_free_mouse, 0, 0, 1, CFGFLAG_CLIENT | CFGFLAG_SAVE, "Free mouse mode (WIP)")
+MACRO_CONFIG_INT(DcUnlockZoom, dc_unlock_zoom, 0, 0, 1, CFGFLAG_CLIENT | CFGFLAG_SAVE, "Disable camera zoom lock")
